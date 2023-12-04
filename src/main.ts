e2d72b3191f1c8901a122721e9c1eaf45ef7362a
nguyenvanduocit+ <nguyenvanduocit@gmail.com>
2023-12-04 23:07:31 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import './style/modern-normalize.css'
+import './style/app.styl'
+
+
+const videoEl = document.querySelector<HTMLVideoElement>('#video')!
+const canvasEl = document.querySelector<HTMLCanvasElement>('#canvas')!
+
+const ctx = canvasEl.getContext('2d')!;
+ctx.filter = 'blur(20px)';
+
+function drawFrame() {
+    ctx.drawImage(videoEl, 0, 0, canvasEl.width, canvasEl.height);
+    requestAnimationFrame(drawFrame);
+}
+
+drawFrame();
+
+const themeToggleButton = document.querySelector<HTMLButtonElement>('#theme-toggle')!;
+themeToggleButton.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark-theme');
+});
+
+const sizeToggleButton = document.querySelector<HTMLButtonElement>('#size-toggle')!;
+sizeToggleButton.addEventListener('click', () => {
+    canvasEl.classList.toggle('full');
+});
+
+const blurSlider = document.querySelector<HTMLInputElement>('#blur-slider')!;
+blurSlider.addEventListener('input', () => {
+    const blurValue = blurSlider.value;
+    ctx.filter = `blur(${blurValue}px)`;
+});
